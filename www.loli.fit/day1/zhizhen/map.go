d5b232d58f979ea733a6f66575ee8081b31b6ec9
桜火 <xie@loli.fit>
2021-04-18 02:04:31 +0800

    az...

--- www.loli.fit/day1/zhizhen/map.go
+++ www.loli.fit/day1/zhizhen/map.go
@@ -5,5 +5,12 @@ import "fmt"
 func main() {
 	ccc := make(map[string]string)
 	ccc["nihao"] = "sj"
+	ccc["didian"] = "成都"
+	ccc["sql"] = "php"
 	fmt.Print(ccc["nihao"])
+	fmt.Print("\n")
+	fmt.Print(ccc["didian"])
+	fmt.Print("\n")
+	fmt.Print(ccc["sql"])
+	fmt.Print("\n")
 }
