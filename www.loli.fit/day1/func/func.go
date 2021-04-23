2e1e53fbaefc65374a9f2146e33f43bb1de7beaa
桜火 <xie@loli.fit>
2021-04-24 02:52:46 +0800

    总结下，我选择跳过map

---
+++ www.loli.fit/day1/func/func.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func chartj(chars *string) (ret int) {
+	mj := *chars
+	num := 0
+	strings := ""
+	for ccc, value := range mj {
+		_ = ccc
+		if unicode.Is(unicode.Han, value) {
+			num++
+			strings = strings + string(value)
+		}
+	}
+	fmt.Printf(strings)
+	return num
+} //执行中文查找并返回中文字符数
+func englishtj(english *string) (ret map[string]int) {
+	yj := *english
+	yj1 := strings.Split(yj, " ")
+	m2 := make(map[string]int, 10)
+	for _, value := range yj1 {
+		if _, ok := m2[value]; !ok {
+			m2[value] = 1
+		} else {
+			m2[value] = m2[value] + 1
+		}
+	}
+	return m2
+} //英文查找并返回数量
+func main() {
+	d := "We present the short story The Romance of a Busy Broker by O Henry The story was originally adapted and recorded by the US Department of State"
+	fmt.Print(englishtj(&d))
+
+}
