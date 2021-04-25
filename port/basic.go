05f2f625d74ab39c14d8fd86aa4f071ed3981042
桜火 <xie@loli.fit>
2021-04-25 21:34:31 +0800

    post与get

---
+++ port/basic.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type data struct {
+	Key string `json:"key"`
+	Num int    `json:"num"`
+}
+
+func get(urls string) {
+
+	gets, err := http.Get(urls)
+	if err != nil {
+		print("错误信息为:", err)
+		panic("发生了内存错误，读取不到对象，")
+	}
+	content, err := ioutil.ReadAll(gets.Body)
+	if err != nil {
+		print("错误信息为:", err)
+		panic("发生了内存错误，读取不到对象，错误信息为:")
+	}
+	fmt.Printf("%s", content)
+}
+func post(url, contenType string) {
+	datas := data{
+		Key: "",
+		Num: 5,
+	}
+	datass, err := json.Marshal(datas)
+	post, err := http.Post(url, contenType, strings.NewReader(string(datass)))
+	if err != nil {
+		print("错误信息为:", err)
+		panic("发生了内存错误，读取不到对象，")
+	}
+	content, err := ioutil.ReadAll(post.Body)
+	fmt.Printf("%s", content)
+}
+func main() {
+	post("https://bduss.loli.fit/pixiv.php", "application/json")
+}
