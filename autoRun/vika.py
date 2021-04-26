a56ef90dce8a70ea9fa4a327d8a9e62759a8be7d
桜火 <xie@loli.fit>
2021-04-26 22:42:32 +0800

    测试提交

--- autoRun/vika.py
+++ autoRun/vika.py
@@ -1,13 +1,3 @@
-#!/usr/bin/python3.7
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2021 桜火, Inc. All Rights Reserved 
-#
-# @Time    : 2021/4/26 21:35
-# @Author  : 桜火
-# @Email   : xie@loli.fit
-# @File    : aba.py
-# @Software: PyCharm
 from vika import Vika
 vika = Vika("uskK7MnF6tOf89ammoV851g")
 dst = vika.datasheet("https://vika.cn/space/spcNuHrHJgQVZ/workbench/dstXHYNcifpEsJuMjZ/viwvYeltzpZ5Z")
