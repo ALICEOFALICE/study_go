33a6fa6ccc6caccb73e2faeb7e5a406ac709b52c
桜火 <xie@loli.fit>
2021-04-26 23:09:55 +0800

    测试提交

--- autoRun/weige.py
+++ autoRun/weige.py
@@ -2,4 +2,4 @@ from vika import Vika
 vika = Vika("uskK7MnF6tOf89ammoV851g")
 dst = vika.datasheet("https://vika.cn/space/spcNuHrHJgQVZ/workbench/dstXHYNcifpEsJuMjZ/viwvYeltzpZ5Z")
 record = dst.records.create({"提交ID":"new record from Python SDK"})
-print(GITHUB_EVENT_PATH)
+print(PR_TITLE)
