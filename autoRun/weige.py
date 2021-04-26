92123e55d83f17482c5e4e1e34b1cc8bcc6a8db7
桜火 <xie@loli.fit>
2021-04-26 23:18:18 +0800

    测试提交

--- autoRun/weige.py
+++ autoRun/weige.py
@@ -1,5 +1,18 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 桜火, Inc. All Rights Reserved 
+#
+# @Time    : 2021/4/26 21:35
+# @Author  : 桜火
+# @Email   : xie@loli.fit
+# @File    : aba.py
+# @Software: PyCharm
 from vika import Vika
+import os
+if __name__ == '__main__':
+    PR_TITLE = os.environ["PR_TITLE"]
 vika = Vika("uskK7MnF6tOf89ammoV851g")
 dst = vika.datasheet("https://vika.cn/space/spcNuHrHJgQVZ/workbench/dstXHYNcifpEsJuMjZ/viwvYeltzpZ5Z")
-record = dst.records.create({"提交ID":"new record from Python SDK"})
 print(PR_TITLE)
+record = dst.records.create({"提交ID":"PR_TITLE"})
