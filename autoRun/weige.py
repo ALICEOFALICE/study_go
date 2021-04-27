35bf786f460d6254e80e8d5e66386189f30d29eb
桜火 <xie@loli.fit>
2021-04-27 18:21:45 +0800

    测试推送

--- autoRun/weige.py
+++ autoRun/weige.py
@@ -15,19 +15,12 @@ json_file = open("/home/runner/work/_temp/_github_workflow/event.json",mode="r")
 json_file = json_file.read()
 env = json.loads(json_file)
 vika = Vika("uskK7MnF6tOf89ammoV851g")
-dst = vika.datasheet("https://vika.cn/space/spcNuHrHJgQVZ/workbench/dstn3TYmEbWgl3EYbQ/viwQn2SSxT225")
+dst = vika.datasheet("dstn3TYmEbWgl3EYbQ")
 
 vika_upadta_dagta = {
 	"提交者：": env["commits"][0]["author"]["name"],
 	"邮箱：": env["commits"][0]["author"]["email"],
 	"提交者用户名：": env["commits"][0]["author"]["username"],
-	"提交注释：": env["commits"][0]["message"],
-	"commit_ID": str(env["commits"][0]["id"]),
-	"提交时间：": env["commits"][0]["timestamp"],
-	"仓库名称：": env["repository"]["name"],
-	"仓库简介：": env["repository"]["description"],
-	"编程语言：": env["repository"]["language"],
-	"主分支：": env["repository"]["master_branch"],
-	"链接：": env["repository"]["html_url"],
+
 }
 record = dst.records.create(vika_upadta_dagta)
