dbafcc109b307a2c829fe13451f2db66a07532e6
桜火 <xie@loli.fit>
2021-04-27 10:37:42 +0800

    测试提交

--- www.loli.fit/day1/struct.go
+++ www.loli.fit/day1/struct.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-
-	fmt.Print("id1")
+	for i := 1; i < 9; i++ {
+		for s := 1; s < 9; s++ {
+			if i < s {
+				break
+			}
+			fmt.Printf("%d X %d = %d \t\t", i, s, i*s)
+		}
+		fmt.Print("\n")
+	}
 }
