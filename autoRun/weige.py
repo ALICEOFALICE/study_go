e4134804cb06dc2b272425be6554936069230269
桜火 <xie@loli.fit>
2021-04-27 10:55:48 +0800

    测试提交

--- autoRun/weige.py
+++ autoRun/weige.py
@@ -13,9 +13,12 @@ from vika import Vika
 import os
 json_file = open("/home/runner/work/_temp/_github_workflow/event.json",mode="r")
 json_file = json_file.read()
-env = json.loads(json_file)
+env = json.loads()
 vika = Vika("uskK7MnF6tOf89ammoV851g")
 dst = vika.datasheet("https://vika.cn/space/spcNuHrHJgQVZ/workbench/dstXHYNcifpEsJuMjZ/viwvYeltzpZ5Z")
-print(env)
+print("仓库名称："+env["description"])
+print("使用语言:"env["language"])
+print(env["description"])
+
 print(type(env))
 # record = dst.records.create({"提交ID":})
